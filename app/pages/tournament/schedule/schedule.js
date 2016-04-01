5ebe7ebcedc2b64dfad38dc0c9de043da06699c4
Samuel-Pierce <sam.pierce@mesh01.com>
2016-04-01 17:43:23 -0400

    working

--- app/pages/tournament/schedule/schedule.js
+++ app/pages/tournament/schedule/schedule.js
@@ -5,11 +5,86 @@ import {Page} from 'ionic-angular';
 })
 
 export class SchedulePage {
-  this.filters = [
-    { title: 'Day', value: ''},
-    { title: 'Division', value: ''},
-    { title: 'Pool', value: '',},
-    { title: 'Grade', value: ''},
-    { title: 'Gender', value: ''},
-  ];
-} // export class
+  // static get parameters() {
+  //   return [[NavController], [NavParams]];
+  // }
+  //
+  // constructor(nav, navParams) {
+  //   this.nav = nav;
+  //
+  //   // If we navigated to this page, we will have an item available as a nav param
+  //   this.selectedItem = navParams.get('item');
+
+    // this.game = [
+    //   {
+    //     gameId: '01',
+    //     team1: 'Boston Celtics',
+    //     team2: 'Miami Heat',
+    //     team1Coach: 'Jim Ray',
+    //     team2Coach: 'Roger Herd',
+    //     team1Score: '87',
+    //     team2Score: '88',
+    //     team1Wins: '2',
+    //     team2Wins: '1',
+    //     team1Losses: '0',
+    //     team2Losses: '2',
+    //     location: 'The RIM',
+    //     time: '8:00am',
+    //     date: '10/13/2016',
+    //     ref1: 'Harry Willmore',
+    //     ref2: 'Rick Martez',
+    //   },
+    //   {
+    //     gameId: '02',
+    //     team1: 'Boston Celtics',
+    //     team2: 'Miami Heat',
+    //     team1Coach: 'Jim Ray',
+    //     team2Coach: 'Roger Herd',
+    //     team1Score: '87',
+    //     team2Score: '88',
+    //     team1Wins: '2',
+    //     team2Wins: '1',
+    //     team1Losses: '0',
+    //     team2Losses: '2',
+    //     location: 'The RIM',
+    //     time: '8:00am',
+    //     date: '10/13/2016',
+    //     ref1: 'Harry Willmore',
+    //     ref2: 'Rick Martez',
+    //   },
+    //   {
+    //     gameId: '03',
+    //     team1: 'Boston Celtics',
+    //     team2: 'Miami Heat',
+    //     team1Coach: 'Jim Ray',
+    //     team2Coach: 'Roger Herd',
+    //     team1Score: '87',
+    //     team2Score: '88',
+    //     team1Wins: '2',
+    //     team2Wins: '1',
+    //     team1Losses: '0',
+    //     team2Losses: '2',
+    //     location: 'The RIM',
+    //     time: '8:00am',
+    //     date: '10/13/2016',
+    //     ref1: 'Harry Willmore',
+    //     ref2: 'Rick Martez',
+    //   },
+    // ];
+    //
+    // this.filters = [
+    //   { title: 'Day', value: ''},
+    //   { title: 'Division', value: ''},
+    //   { title: 'Pool', value: '',},
+    //   { title: 'Grade', value: ''},
+    //   { title: 'Gender', value: ''},
+    // ];
+
+    // itemTapped(event, item) {
+    //  this.nav.push(ItemDetailsPage, {
+    //    item: item
+    //  });
+    // }
+
+   // } constructor
+} // class
