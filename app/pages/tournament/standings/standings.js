322c107f0cb6e9e6c0ec6d38f676396320746e4d
Samuel-Pierce <sam.pierce@mesh01.com>
2016-04-05 11:44:50 -0400

    1

--- app/pages/tournament/standings/standings.js
+++ app/pages/tournament/standings/standings.js
@@ -4,4 +4,11 @@ import {Page} from 'ionic-angular';
 @Page({
   templateUrl: 'build/pages/tournament/standings/standings.html'
 })
-export class StandingsPage {}
+export class StandingsPage {
+
+  this.standings = [
+    { team: 'celtics', pos: 1 },
+    { team: 'lakers', pos: 2 },
+    { team: 'heat', pos: 3 }
+  ];
+}
